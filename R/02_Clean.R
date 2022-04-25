7dceb96e664f0906eb300a42b0324cf72b3ab4c8
Boris Blazej <boris.blazejgmx.at>
2022-04-25 18:39:41 +0200

    02_Clean.R to clean the text

---
+++ R/02_Clean.R
@@ -0,0 +1,40 @@
+### Task: load raw_text data as developed in 01_Explore.Rmd from a given corpus and do some transparent cleaning steps
+### Output: clean_text
+
+# 0. Libraries
+library(stringr)
+library(readr)
+
+# 1. Load raw data
+### In case of error run 01_Explore.Rmd
+
+raw_data_path <- "./data/en_US.twitter.txt"
+con <- file(raw_data_path)
+raw_text <- readLines(con)
+
+head(raw_text, 10)
+
+# 2. Remove "non-word" characters
+
+text_1 <- str_replace_all(raw_text, "[^\\w]", " ")
+text_1 <- str_replace_all(text_1, "[\\s]+", " ")
+text_1 <- str_replace_all(text_1, "[^A-Za-z0-9]", " ")
+
+head(text_1, 10)
+
+# 3. All characters lowercase
+
+clean_text <- str_to_lower(text_1)
+
+sample(clean_text, 100)
+
+
+
+# 99. Write RDS to use in furtehr steps
+
+write_rds(clean_text, "./data/clean_text.rds.gz", compress = "gz")
+
+
+
+
+
