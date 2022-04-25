c14d785cc08fa204c2c15f5385555d6f4d93ed18
Boris Blazej <boris.blazejgmx.at>
2022-04-25 19:55:37 +0200

    03_Explore_Frequencies.Rmd: first experiments with frequencies

--- R/02_Clean.R
+++ R/02_Clean.R
@@ -19,6 +19,7 @@ head(raw_text, 10)
 text_1 <- str_replace_all(raw_text, "[^\\w]", " ")
 text_1 <- str_replace_all(text_1, "[\\s]+", " ")
 text_1 <- str_replace_all(text_1, "[^A-Za-z0-9]", " ")
+text_1 <- str_trim(text_1)
 
 head(text_1, 10)
 
@@ -32,7 +33,7 @@ sample(clean_text, 100)
 
 # 99. Write RDS to use in furtehr steps
 
-write_rds(clean_text, "./data/clean_text.rds.gz", compress = "gz")
+write_rds(clean_text, "./data/en_US.twitter_clean.rds.gz", compress = "gz")
 
 
 
