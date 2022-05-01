33d59a368090e4b22d9595f879cc889835678741
Boris Blazej <boris.blazej@gmx.at>
2022-05-01 20:12:24 +0200

    model structure (week 3)

---
+++ R/swiftkey1.R
@@ -0,0 +1,36 @@
+# Implementation of an n-gram Katz model, version 1
+
+# 
+
+# Public Functions ----------------------------------------
+
+load_text <- function(textfile) {
+    
+}
+
+
+clean_text <- function(text) {
+    
+}
+
+train_model <- function(text) {
+    
+    ngrams <- list()
+    for(n in 1:4) {
+        ngram[[n]] <- build_ngram(text, n)
+    }
+    
+    
+    return(model)
+}
+
+next_word <- function(phrase) {
+    
+}
+
+# Internal Functions ----------------------------------------
+
+## for train_model()
+build_ngram <- function(text, n) {
+    
+}
